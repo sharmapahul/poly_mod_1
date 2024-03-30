e5dd44d73bfa52903c1b7c921fca3a4e2c07b7fc
sharmapahul <165384030+sharmapahul@users.noreply.github.com>
2024-03-30 17:48:54 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,22 +1,18 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const contractAddress = "0xDba1Ec5BA4963845Be7f59c1C80672a5Cf3a3140";
-  const receiverAddress = "0xbEEF5F20BCd8Cb86da852F648690AeAaDCb3C277";
+  const contractAddress = "0x6B28e97Fd505d759F4dB115C41398D8A384eE3Ce";
+  const receiverAddress = "0x9dB01459c498fB5D18b23d537e1A50A749CeF092";
   const quantity = 2;
   const value = ethers.parseEther("0.03");
-  const baseURI =
-    "https://gateway.pinata.cloud/ipfs/QmarShHg1QbwttVuXPSdjzyGMCx1R6ynj3hBUBU2paDZNC/";
 
-  const batchNFTs = await ethers.getContractAt("BatchNFTs", contractAddress);
-
-  await batchNFTs.setBaseURI(baseURI);
-  const mintTokens = await batchNFTs.mint(receiverAddress, quantity, {
+  const NFTs = await ethers.getContractAt("antartica2", contractAddress);
+  const mintTokens = await NFTs.mint(receiverAddress, quantity, {
     value: value,
   });
 
   console.log(
-    `Transaction Hash: https://sepolia.etherscan.io/tx/${mintTokens.hash}`
+    `Transaction Hash: ${mintTokens.hash}`
   );
 }
 
