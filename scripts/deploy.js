783d5da1c8cc3db6c01d50433329e9c1fcd11d03
sharmapahul <165384030+sharmapahul@users.noreply.github.com>
2024-03-30 17:48:32 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,14 +4,11 @@ async function main() {
   const latestBlock = await hre.ethers.provider.getBlock("latest");
   //const add100BlocksToCurrent = latestBlock.timestamp + 1000;
 
-  const BatchNFTs = await hre.ethers.getContractFactory("BatchNFTs");
+  const BatchNFTs = await hre.ethers.getContractFactory("antartica2");
   const batchNFTs = await BatchNFTs.deploy(latestBlock.timestamp, false);
 
   await batchNFTs.waitForDeployment(latestBlock.timestamp);
 
-  console.log(
-    `Deploy ERC721A contract and schedule mint to open on block ${latestBlock.timestamp}`
-  );
 
   const address = await batchNFTs.getAddress();
   console.log(`Contract Address: ${address}`);
